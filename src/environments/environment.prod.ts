a766786faddc49c369bf4966c22754e7baabe8ef
sachin gawai <sachingawai07@gmail.com>
2022-08-07 15:11:59 +0530

    add environment for production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,10 @@
 export const environment = {
-  production: true
+  production: true,
+  token:"b157dc97-b14b-4871-b245-271cbc68723c",
+    baseUrl:"https://api.thecatapi.com/v1/",
+    catList: "v1/images/search",
+    favouritesCatList:"v1/favourites",
+    voteIt:"v1/votes",
+    upload:"v1/images/upload",
+    myCatlist:"v1/images" 
 };
